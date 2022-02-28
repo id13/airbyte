3b0a480193f4a4f046f3a2bb92bbec7c8947670b
alafanechere <augustin.lafanechere@gmail.com>
2022-02-28 23:05:15 +0100

    test diff_helpers.py

--- octavia-cli/octavia_cli/apply/diff_helpers.py
+++ octavia-cli/octavia_cli/apply/diff_helpers.py
@@ -3,16 +3,25 @@
 #
 
 import hashlib
+from typing import Any
 
 from deepdiff import DeepDiff
 
 SECRET_MASK = "**********"
 
 
-def compute_checksum(configuration_path):
+def compute_checksum(file_path: str) -> str:
+    """Compute SHA256 checksum from a file
+
+    Args:
+        file_path (str): Path for the file for which you want to compute a checksum.
+
+    Returns:
+        str: The computed hash digest
+    """
     BLOCK_SIZE = 65536
     file_hash = hashlib.sha256()
-    with open(configuration_path, "rb") as f:
+    with open(file_path, "rb") as f:
         fb = f.read(BLOCK_SIZE)
         while len(fb) > 0:
             file_hash.update(fb)
@@ -20,12 +29,30 @@ def compute_checksum(configuration_path):
     return file_hash.hexdigest()
 
 
-def exclude_secrets_from_diff(obj, path):
+def exclude_secrets_from_diff(obj: Any, path: str) -> bool:
+    """Callback function use with DeepDiff to ignore secret value from the diff.
+
+    Args:
+        obj (Any): Object for which a diff will be computed.
+        path (str): unused.
+
+    Returns:
+        bool: Wetheir to ignore the object from the diff.
+    """
     if isinstance(obj, str):
         return True if SECRET_MASK in obj else False
     else:
         return False
 
 
-def compute_diff(a, b):
+def compute_diff(a: Any, b: Any) -> DeepDiff:
+    """Wrapper around the DeepDiff computation.
+
+    Args:
+        a (Any): Object to compare with b.
+        b (Any): Object to compare with a.
+
+    Returns:
+        DeepDiff: the computed diff object.
+    """
     return DeepDiff(a, b, view="tree", exclude_obj_callback=exclude_secrets_from_diff)
