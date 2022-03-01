fe47a7876bb2d422a207f1979a22c4e85cd16f51
alafanechere <augustin.lafanechere@gmail.com>
2022-03-01 11:00:02 +0100

    wip test

--- octavia-cli/octavia_cli/apply/resources.py
+++ octavia-cli/octavia_cli/apply/resources.py
@@ -6,15 +6,19 @@ import abc
 import os
 import time
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable, Optional, Union
 
 import airbyte_api_client
 import yaml
 from airbyte_api_client.api import destination_api, source_api
 from airbyte_api_client.model.destination_create import DestinationCreate
+from airbyte_api_client.model.destination_read import DestinationRead
+from airbyte_api_client.model.destination_read_list import DestinationReadList
 from airbyte_api_client.model.destination_search import DestinationSearch
 from airbyte_api_client.model.destination_update import DestinationUpdate
 from airbyte_api_client.model.source_create import SourceCreate
+from airbyte_api_client.model.source_read import SourceRead
+from airbyte_api_client.model.source_read_list import SourceReadList
 from airbyte_api_client.model.source_search import SourceSearch
 from airbyte_api_client.model.source_update import SourceUpdate
 from click import ClickException
@@ -26,8 +30,50 @@ class DuplicateRessourceError(ClickException):
     pass
 
 
+class NonExistingRessourceError(ClickException):
+    pass
+
+
 class InvalidConfigurationError(ClickException):
     pass
+
+
+class ResourceState:
+    def __init__(self, configuration_path, resource_id, generation_timestamp, configuration_checksum):
+        self.configuration_path = configuration_path
+        self.resource_id = resource_id
+        self.generation_timestamp = generation_timestamp
+        self.configuration_checksum = configuration_checksum
+        self.path = os.path.join(os.path.dirname(self.configuration_path), "state.yaml")
+
+    def _save(self):
+        content = {
+            "configuration_path": self.configuration_path,
+            "resource_id": self.resource_id,
+            "generation_timestamp": self.generation_timestamp,
+            "configuration_checksum": self.configuration_checksum,
+        }
+        with open(self.path, "w") as state_file:
+            yaml.dump(content, state_file)
+
+    @classmethod
+    def create(cls, configuration_path, resource_id):
+        generation_timestamp = int(time.time())
+        configuration_checksum = compute_checksum(configuration_path)
+        state = ResourceState(configuration_path, resource_id, generation_timestamp, configuration_checksum)
+        state._save()
+        return state
+
+    @classmethod
+    def from_file(cls, file_path):
+        with open(file_path, "r") as f:
+            raw_state = yaml.load(f, yaml.FullLoader)
+        return ResourceState(
+            raw_state["configuration_path"],
+            raw_state["resource_id"],
+            raw_state["generation_timestamp"],
+            raw_state["configuration_checksum"],
+        )
 
 
 class BaseResource(abc.ABC):
@@ -120,7 +166,7 @@ class BaseResource(abc.ABC):
         )
 
     def __getattr__(self, name: str) -> Any:
-        """Map attribute of the YAML config to the AirbyteResource object.
+        """Map attribute of the YAML config to the Resource object.
 
         Args:
             name (str): Attribute name
@@ -135,51 +181,108 @@ class BaseResource(abc.ABC):
             return self.local_configuration.get(name)
         raise AttributeError(f"{self.__class__.__name__}.{name} is invalid.")
 
-    def _search(self):
+    def _search(self) -> Union[SourceReadList, DestinationReadList]:
+        """Run search of a resources on the remote Airbyte instance.
+
+        Returns:
+            Union[SourceReadList, DestinationReadList]: Search results
+        """
         return self._search_fn(self.api_instance, self.search_payload)
 
-    def _get_state_from_file(self):
+    def _get_state_from_file(self) -> Optional[ResourceState]:
+        """Retrieve a state object from a local YAML file if it exists.
+
+        Returns:
+            Optional[ResourceState]: the deserialized resource state if YAML file found.
+        """
         expected_state_path = Path(os.path.join(os.path.dirname(self.configuration_path), "state.yaml"))
         if expected_state_path.is_file():
             return ResourceState.from_file(expected_state_path)
 
-    def _get_remote_resource(self):
+    def _get_remote_resource(self) -> Optional[Union[SourceRead, DestinationRead]]:
+        """Find the remote resource on the Airbyte instance associated with the current resource.
+
+        Raises:
+            DuplicateRessourceError: raised if the search results return multiple resources.
+
+        Returns:
+            Optional[Union[SourceRead, DestinationRead]]: The remote resource found.
+        """
         search_results = self._search().get(f"{self.resource_type}s", [])
         if len(search_results) > 1:
-            raise DuplicateRessourceError("Two or more ressource exist with the same name")
+            raise DuplicateRessourceError("Two or more ressource exist with the same name.")
         if len(search_results) == 1:
             return search_results[0]
         else:
             return None
 
-    def get_diff_with_remote_resource(self):
+    def get_diff_with_remote_resource(self) -> str:
+        """Compute the diff between current resource and the remote resource.
+
+        Raises:
+            NonExistingRessourceError: Raised if the remote resource does not exist.
+
+        Returns:
+            str: The prettyfied diff.
+        """
+        if not self.was_created:
+            raise NonExistingRessourceError("Cannot compute diff with a non existing remote resource.")
         current_config = self.configuration
-        if self.was_created:
-            remote_config = self.remote_resource.connection_configuration
+        remote_config = self.remote_resource.connection_configuration
         diff = compute_diff(remote_config, current_config)
         return diff.pretty()
 
-    def _create_or_update(self, operation_fn, payload):
+    def _create_or_update(
+        self, operation_fn: Callable, payload: Union[SourceCreate, SourceUpdate, DestinationCreate, DestinationUpdate]
+    ) -> Union[SourceRead, DestinationRead]:
+        """Wrapper to trigger create or update of remote resource.
+
+        Args:
+            operation_fn (Callable): The API function to run.
+            payload (Union[SourceCreate, SourceUpdate, DestinationCreate, DestinationUpdate]): The payload to send to create or update the resource.
+
+        Raises:
+            InvalidConfigurationError: Raised if the create or update payload is invalid.
+            ApiException: Raised in case of other API errors.
+
+        Returns:
+            Union[SourceRead, DestinationRead]: The created or updated resource.
+        """
         try:
             result = operation_fn(self.api_instance, payload)
             return result, ResourceState.create(self.configuration_path, result[self.resource_id_field])
-        except airbyte_api_client.ApiException as e:
-            if e.status == 422:
+        except airbyte_api_client.ApiException as api_error:
+            if api_error.status == 422:
                 # This  API response error is really verbose, but it embodies all the details about why the config is not valid.
                 # TODO alafanechere: try to parse it and display it in a more readable way.
-                raise InvalidConfigurationError(e.body)
+                raise InvalidConfigurationError(api_error.body)
             else:
-                raise e
+                raise api_error
 
-    def create(self):
+    def create(self) -> Union[SourceRead, DestinationRead]:
+        """Public function to create the resource on the remote Airbyte instance.
+
+        Returns:
+            Union[SourceRead, DestinationRead]: The created resource.
+        """
         return self._create_or_update(self._create_fn, self.create_payload)
 
-    def update(self):
+    def update(self) -> Union[SourceRead, DestinationRead]:
+        """Public function to update the resource on the remote Airbyte instance.
+
+        Returns:
+            Union[SourceRead, DestinationRead]: The updated resource.
+        """
         return self._create_or_update(self._update_fn, self.update_payload)
 
     @property
-    def resource_id(self):
-        return self.remote_resource.get(self.resource_id_field)
+    def resource_id(self) -> Optional[str]:
+        """Exposes the resource UUID of the remote resource
+
+        Returns:
+            str: Remote resource's UUID
+        """
+        return self.remote_resource.get(self.resource_id_field) if self.was_created else None
 
 
 class Source(BaseResource):
@@ -249,41 +352,3 @@ def factory(api_client, workspace_id, configuration_path):
     if local_configuration["definition_type"] == "destination":
         AirbyteResource = Destination
     return AirbyteResource(api_client, workspace_id, local_configuration, configuration_path)
-
-
-class ResourceState:
-    def __init__(self, configuration_path, resource_id, generation_timestamp, configuration_checksum):
-        self.configuration_path = configuration_path
-        self.resource_id = resource_id
-        self.generation_timestamp = generation_timestamp
-        self.configuration_checksum = configuration_checksum
-        self.path = os.path.join(os.path.dirname(self.configuration_path), "state.yaml")
-
-    def _save(self):
-        content = {
-            "configuration_path": self.configuration_path,
-            "resource_id": self.resource_id,
-            "generation_timestamp": self.generation_timestamp,
-            "configuration_checksum": self.configuration_checksum,
-        }
-        with open(self.path, "w") as state_file:
-            yaml.dump(content, state_file)
-
-    @classmethod
-    def create(cls, configuration_path, resource_id):
-        generation_timestamp = int(time.time())
-        configuration_checksum = compute_checksum(configuration_path)
-        state = ResourceState(configuration_path, resource_id, generation_timestamp, configuration_checksum)
-        state._save()
-        return state
-
-    @classmethod
-    def from_file(cls, file_path):
-        with open(file_path, "r") as f:
-            raw_state = yaml.load(f, yaml.FullLoader)
-        return ResourceState(
-            raw_state["configuration_path"],
-            raw_state["resource_id"],
-            raw_state["generation_timestamp"],
-            raw_state["configuration_checksum"],
-        )
--- octavia-cli/tests/unit/test_apply/test_resources.py
+++ octavia-cli/tests/unit/test_apply/test_resources.py
@@ -3,6 +3,7 @@
 #
 
 import pytest
+from airbyte_api_client import ApiException
 from octavia_cli.apply import resources
 
 
@@ -20,7 +21,7 @@ class TestBaseResource:
 
     @pytest.fixture
     def local_configuration(self):
-        return {"exotic_attribute": "foo"}
+        return {"exotic_attribute": "foo", "configuration": {"foo": "bar"}}
 
     def test_init_no_remote_resource(self, mocker, patch_base_class, mock_api_client, local_configuration):
         mocker.patch.object(resources.BaseResource, "_get_state_from_file", mocker.Mock(return_value=None))
@@ -36,16 +37,19 @@ class TestBaseResource:
         assert resource.remote_resource == resource._get_remote_resource.return_value
         assert resource.was_created == resource._get_remote_resource.return_value
         assert resource.local_file_changed is True
+        assert resource.resource_id is None
 
     def test_init_with_remote_resource_not_changed(self, mocker, patch_base_class, mock_api_client, local_configuration):
         mocker.patch.object(
             resources.BaseResource, "_get_state_from_file", mocker.Mock(return_value=mocker.Mock(configuration_checksum="my_checksum"))
         )
-        mocker.patch.object(resources.BaseResource, "_get_remote_resource", mocker.Mock(return_value=True))
+        mocker.patch.object(resources.BaseResource, "_get_remote_resource", mocker.Mock(return_value={"resource_id": "my_resource_id"}))
+
         mocker.patch.object(resources, "compute_checksum", mocker.Mock(return_value="my_checksum"))
         resource = resources.BaseResource(mock_api_client, "workspace_id", local_configuration, "bar.yaml")
         assert resource.was_created is True
         assert resource.local_file_changed is False
+        assert resource.resource_id == "my_resource_id"
 
     def test_init_with_remote_resource_changed(self, mocker, patch_base_class, mock_api_client, local_configuration):
         mocker.patch.object(
@@ -53,14 +57,108 @@ class TestBaseResource:
             "_get_state_from_file",
             mocker.Mock(return_value=mocker.Mock(configuration_checksum="my_state_checksum")),
         )
-        mocker.patch.object(resources.BaseResource, "_get_remote_resource", mocker.Mock(return_value=True))
+        mocker.patch.object(resources.BaseResource, "_get_remote_resource", mocker.Mock(return_value={"resource_id": "my_resource_id"}))
         mocker.patch.object(resources, "compute_checksum", mocker.Mock(return_value="my_new_checksum"))
         resource = resources.BaseResource(mock_api_client, "workspace_id", local_configuration, "bar.yaml")
         assert resource.was_created is True
         assert resource.local_file_changed is True
+        assert resource.resource_id == "my_resource_id"
 
-    def test_get_attr(self, patch_base_class, mock_api_client, local_configuration):
-        resource = resources.BaseResource(mock_api_client, "workspace_id", local_configuration, "bar.yaml")
+    @pytest.fixture
+    def resource(self, patch_base_class, mock_api_client, local_configuration):
+        return resources.BaseResource(mock_api_client, "workspace_id", local_configuration, "bar.yaml")
+
+    def test_get_attr(self, resource, local_configuration):
         assert resource.exotic_attribute == local_configuration["exotic_attribute"]
         with pytest.raises(AttributeError):
             resource.wrong_attribute
+
+    def test_search(self, resource):
+        search_results = resource._search()
+        assert search_results == resource._search_fn.return_value
+        resource._search_fn.assert_called_with(resource.api_instance, resource.search_payload)
+
+    @pytest.mark.parametrize(
+        "search_results,expected_error,expected_output",
+        [
+            ([], None, None),
+            (["foo"], None, "foo"),
+            (["foo", "bar"], resources.DuplicateRessourceError, None),
+        ],
+    )
+    def test_get_remote_resource(self, resource, mocker, search_results, expected_error, expected_output):
+        mock_search_results = mocker.Mock(return_value=search_results)
+        mocker.patch.object(resource, "_search", mocker.Mock(return_value=mocker.Mock(get=mock_search_results)))
+        if expected_error is None:
+            remote_resource = resource._get_remote_resource()
+            assert remote_resource == expected_output
+        else:
+            with pytest.raises(expected_error):
+                remote_resource = resource._get_remote_resource()
+        resource._search.return_value.get.assert_called_with("universal_resources", [])
+
+    @pytest.mark.parametrize(
+        "state_path_is_file",
+        [True, False],
+    )
+    def test_get_state_from_file(self, mocker, resource, state_path_is_file):
+        mocker.patch.object(resources, "os")
+        mock_expected_state_path = mocker.Mock(is_file=mocker.Mock(return_value=state_path_is_file))
+        mocker.patch.object(resources, "Path", mocker.Mock(return_value=mock_expected_state_path))
+        mocker.patch.object(resources, "ResourceState")
+        state = resource._get_state_from_file()
+        resources.os.path.dirname.assert_called_with(resource.configuration_path)
+        resources.os.path.join.assert_called_with(resources.os.path.dirname.return_value, "state.yaml")
+        resources.Path.assert_called_with(resources.os.path.join.return_value)
+        if state_path_is_file:
+            resources.ResourceState.from_file.assert_called_with(mock_expected_state_path)
+            assert state == resources.ResourceState.from_file.return_value
+        else:
+            assert state is None
+
+    @pytest.mark.parametrize(
+        "was_created",
+        [True, False],
+    )
+    def test_get_diff_with_remote_resource(self, mocker, resource, was_created):
+        mocker.patch.object(resource, "was_created", was_created)
+        mock_remote_resource = mocker.Mock()
+        mocker.patch.object(resource, "remote_resource", mock_remote_resource)
+        mocker.patch.object(resources, "compute_diff")
+        if was_created:
+            diff = resource.get_diff_with_remote_resource()
+            resources.compute_diff.assert_called_with(resource.remote_resource.connection_configuration, resource.configuration)
+            assert diff == resources.compute_diff.return_value.pretty.return_value
+        else:
+            with pytest.raises(resources.NonExistingRessourceError):
+                resource.get_diff_with_remote_resource()
+
+    def test_create_or_update(self, mocker, resource):
+        expected_results = {resource.resource_id_field: "resource_id"}
+        operation_fn = mocker.Mock(return_value=expected_results)
+        mocker.patch.object(resources, "ResourceState")
+        payload = "foo"
+        result, state = resource._create_or_update(operation_fn, payload)
+        assert result == expected_results
+        assert state == resources.ResourceState.create.return_value
+        resources.ResourceState.create.assert_called_with(resource.configuration_path, "resource_id")
+
+    @pytest.mark.parametrize(
+        "response_status,expected_error",
+        [(404, ApiException), (422, resources.InvalidConfigurationError)],
+    )
+    def test_create_or_update_error(self, mocker, resource, response_status, expected_error):
+        operation_fn = mocker.Mock(side_effect=ApiException(status=response_status))
+        mocker.patch.object(resources, "ResourceState")
+        with pytest.raises(expected_error):
+            resource._create_or_update(operation_fn, "foo")
+
+    def test_create(self, mocker, resource):
+        mocker.patch.object(resource, "_create_or_update")
+        assert resource.create() == resource._create_or_update.return_value
+        resource._create_or_update.assert_called_with(resource._create_fn, resource.create_payload)
+
+    def test_update(self, mocker, resource):
+        mocker.patch.object(resource, "_create_or_update")
+        assert resource.update() == resource._create_or_update.return_value
+        resource._create_or_update.assert_called_with(resource._update_fn, resource.update_payload)
